7b01fa622aa4d4ce85b7b8433830275c159bfb78
phuc.pham3 <phuc.pham3>
2023-06-11 16:00:51 +0700

    new update rate

--- src/router/api/rate/controllers/update_rate.ts
+++ src/router/api/rate/controllers/update_rate.ts
@@ -2,11 +2,23 @@ import { Request, Response } from "express";
 import RateSchema from "@/models/rate";
 
 export const updateRate = async (req: Request, res: Response) => {
-  const {} = req.body as any;
+  const { name, fee, transactions, atmDeposits, addOns, books } =
+    req.body as any;
   try {
-    const newData = new RateSchema({});
-    const dataNew = await newData.save();
-    return res.status(200).json({ status: "ok", data: dataNew.doc() });
+    const newRate = await RateSchema.findOneAndUpdate(
+      { name },
+      {
+        $set: {
+          fee,
+          transactions,
+          atmDeposits,
+          addOns,
+          books,
+        },
+      },
+      { new: true }
+    );
+    return res.status(200).json({ status: "ok", data: newRate?.doc() });
   } catch (error) {
     return res.status(400).json({ status: 400, error });
   }
