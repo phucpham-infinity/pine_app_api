5cfeee2b32fcee0e0fe8c82e1384d591f4f8af02
phuc.pham3 <phuc.pham3>
2023-06-16 21:52:33 +0700

    Update error company error

--- src/models/company/index.ts
+++ src/models/company/index.ts
@@ -11,6 +11,7 @@ export interface ICompany extends IBaseModal {
   licenseNumber?: string;
   backupEmail?: string;
   coverUrl?: string;
+  status: "PROCESSING" | "VERIFIED" | "DELIVERY" | "DONE";
 }
 
 const CompanySchema = createSchema({
@@ -22,6 +23,7 @@ const CompanySchema = createSchema({
   licenseNumber: { type: String, required: false, default: "" },
   backupEmail: { type: String, required: false, default: "" },
   coverUrl: { type: String, required: false },
+  status: { type: String, required: false, default: "PROCESSING" },
 });
 
 CompanySchema.method("doc", function () {
--- src/router/api/company/controllers/create_company.ts
+++ src/router/api/company/controllers/create_company.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import CompanySchema from "@/models/company";
 import { ICompany } from "@/models/company";
+import { keys } from "lodash";
 
 export const createCompany = async (req: Request, res: Response) => {
   const {
@@ -28,7 +29,14 @@ export const createCompany = async (req: Request, res: Response) => {
   try {
     const newCompany2 = await newCompany.save();
     return res.status(200).json({ status: "ok", data: newCompany2.doc() });
-  } catch (error) {
+  } catch (error: any) {
+    console.log("error", error);
+    if (error.code === 11000) {
+      return res.status(400).json({
+        status: 400,
+        error: `This ${keys(error?.keyPattern)?.[0]} is unavailable`,
+      });
+    }
     return res.status(400).json({ status: 400, error });
   }
 };
