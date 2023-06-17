6fddbde7ac4a37898fdb443978b5378d80e79ef2
phuc.pham3 <phuc.pham3>
2023-06-17 18:04:09 +0700

    add Upsert setting

--- src/router/api/setting/controllers/update_setting.ts
+++ src/router/api/setting/controllers/update_setting.ts
@@ -38,8 +38,21 @@ export const updateSetting = async (req: Request, res: Response) => {
       { new: true }
     );
 
-    if (!newData)
-      return res.status(400).json({ status: 400, error: "Data not found!" });
+    if (!newData) {
+      const newData2 = new SettingSchema({
+        userId: _id,
+        confirmationMethods,
+        enableBiometric,
+        notificationMethods,
+        receiveNotificationsForDeposits,
+        receiveNotificationsForExceedingSetLimits,
+        receiveNotificationsForOutstandingInvoices,
+        receiveNotificationsForPaymentsAndTransfers,
+        mainAccountId,
+      });
+      const dataNew2 = await newData2.save();
+      return res.status(200).json({ status: "ok", data: dataNew2.doc() });
+    }
     return res.status(200).json({ status: "ok", data: newData?.doc() });
   } catch (error) {
     return res.status(400).json({ status: 400, error });
